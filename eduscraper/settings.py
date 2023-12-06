421504b09cc5ee23f86d6d3d3dde4c5c4099e4ec
Fardeen Ehsan <fardeen.es7@gmail.com>
2023-12-06 20:34:51 +0600

    static root settings

--- eduscraper/settings.py
+++ eduscraper/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
