271912cd73d2862968f08b5ae0d0f595d3b65a07
Fardeen Ehsan <fardeen.es7@gmail.com>
2023-12-06 20:18:36 +0600

    debug mode

--- eduscraper/settings.py
+++ eduscraper/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if OS.environ.get('DEBUG')=="True" else False
+DEBUG = True if os.environ.get('DEBUG')=="True" else False
 
 ALLOWED_HOSTS = ['.vercel.app', '.fardiin.com']
 
