db0fe1fbd1137311165d5e265842efc5a4529be0
Fardeen Ehsan <fardeen.es7@gmail.com>
2023-12-06 21:07:27 +0600

    host settings

--- eduscraper/settings.py
+++ eduscraper/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG')=="True" else False
 
-ALLOWED_HOSTS = ['eduscraper.vercel.app', 'eduscraper.fardiin.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.fardiin.com', '127.0.0.1']
 
 
 # Application definition
